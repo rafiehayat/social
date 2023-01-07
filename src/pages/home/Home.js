49ad63abb712ba9ed9e0ceec3ec5a63a31b96227
Hayat <rafiehayat5@gmail.com>
2023-01-07 14:16:52 +0530

    feedBar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -11,8 +11,8 @@ export default function Home() {
       <Topbar />
       <div className="homeContainer">
         <Sidebar />
-        <Rightbar />
         <Feed />
+        <Rightbar />
       </div>
     </>
   );
