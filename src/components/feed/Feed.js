6ed65249410182fdce1e458e550a611b03ffe61a
Hayat <rafiehayat5@gmail.com>
2023-01-18 21:48:22 +0530

    Posting Status with images

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -16,7 +16,11 @@ export default function Feed({ username }) {
         ? await axios.get("/posts/profile/" + username)
         : // : await axios.get("posts/timeline/63bed8e88af6a6ae754fe606");
           await axios.get("posts/timeline/" + user._id);
-      setPosts(res.data);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
     };
     fetchPosts();
   }, [username, user._id]);
